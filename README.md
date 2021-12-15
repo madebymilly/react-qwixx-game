083d19cb765c3228a4eeb11d50ea88c434b6a393
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2021-12-15 13:57:39 +0100

    add hook for toggle state

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 Functional React components, hooks, context, reducers, memo.
 
+Some choices may feel a little superfluous, but I added them anyway because of learning purposes.
+(eg. reusable hook for toggle state)
+
 ## To do:
 
 - XX Show scores per color & total score
@@ -13,7 +16,7 @@ Functional React components, hooks, context, reducers, memo.
 - XX add context for dice (no need for reducer & memo)
 - XX add StartForm 
 - XX let player change it's name
-- add reusable hook for toggle state
+- XX add reusable hook for toggle state
 - add reusable hook for input state
 - switch player turn
 - make last box clickable when: at least 5 boxes checked in same row.
--- src/Player.js
+++ src/Player.js
@@ -2,34 +2,31 @@ import React, { memo, useContext, useState } from 'react'
 import Card from './Card'
 import './styling/Player.css'
 import { DispatchContext } from './context/PlayersContext';
+import useToggleState from './hooks/useToggleState';
 
 function Player(props) {
   const { id, name, boxes, penalties} = props;
   const dispatch = useContext(DispatchContext);
-  const [editable, toggleEditable] = useState(false);
+  const [inputShown, toggleInput] = useToggleState(false);
   const [newName, setNewName] = useState(name);
 
   const handleChange = (e) => {
     setNewName(e.target.value);
   }
 
-  const handleEditName = () => {
-    toggleEditable(!editable);
-  }
-
   const handleSubmit = () => {
     dispatch({type: 'CHANGE_PLAYER_NAME', playerId: id, name: newName });
-    toggleEditable(!editable);
+    toggleInput();
   }
 
   return (
     <div className="Player">
-      {!editable
-        ? <h3 className="Player__name" onClick={handleEditName}>{name}</h3>
-        : <form className="Editform" onSubmit={handleSubmit}>
+      {inputShown
+        ? <form className="Editform" onSubmit={handleSubmit}>
             <input type="text" id="playerName" name="playerName" value={newName} onChange={handleChange} />
             <button className="Secondary-button">Edit</button>
           </form>
+        : <h3 className="Player__name" onClick={toggleInput}>{name}</h3>
       }
       <Card boxes={boxes} penalties={penalties} playerId={id} />
     </div>
